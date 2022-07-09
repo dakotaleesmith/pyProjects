a11af303ca09cd14f813535acf9c9ce7f991d673
dakotaleesmith <dakotaleesmith@gmail.com>
2022-07-09 19:58:57 -0400

    Got rid of a period

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -23,6 +23,6 @@ class AlienInvasion:
             pygame.display.flip()
 
 if __name__ == '__main__':
-    # Make a game instance and run the game.
+    # Make a game instance and run the game
     ai = AlienInvasion()
     ai.run_game()
