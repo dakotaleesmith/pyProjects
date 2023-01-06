b16e8cf293da24b33b5638b9449446b06764bc5a
dakotaleesmith <dakotaleesmith@gmail.com>
2023-01-05 18:24:34 -0600

    Edited an incorrect type hint that said get_color_value returned a json rather than a dict

--- zsh_python_tutorial/get_color_value.py
+++ zsh_python_tutorial/get_color_value.py
@@ -17,12 +17,12 @@ from sys import argv
 from insert_data import file_path
 
 
-def get_data():
+def get_data() -> json:
     with open(file_path, "r") as f:
         return json.load(f)
 
 
-def get_color_value(color: str) -> json:
+def get_color_value(color: str) -> dict:
     data = get_data()
     assert color in [
         item.get("color") for item in data
