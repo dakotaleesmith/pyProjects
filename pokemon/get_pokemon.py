41c59edc70901804aadd923968e276cc98996dd8
dakotaleesmith <dakotaleesmith@gmail.com>
2023-03-12 21:34:19 -0700

    Adjusted _format_results method to print type effectiveness results in more organized format in Terminal

--- pokemon/get_pokemon.py
+++ pokemon/get_pokemon.py
@@ -32,8 +32,8 @@ def _get_type_effectiveness_query(types: list) -> str:
         SELECT
             type1.opposing_type
             , COALESCE(type1.effectiveness_multiplier * type2.effectiveness_multiplier, 0) AS effectiveness
-        FROM types_eff AS type1
-        INNER JOIN types_eff AS type2
+        FROM type_eff AS type1
+        INNER JOIN type_eff AS type2
             ON type1.opposing_type = type2.opposing_type
             AND type1.type != type2.type
         GROUP BY 1, 2
@@ -45,13 +45,13 @@ def _get_type_effectiveness_query(types: list) -> str:
         SELECT
             type1.opposing_type
             , COALESCE(type1.effectiveness_multiplier, 0) AS effectiveness
-        FROM types_eff AS type1
+        FROM type_eff AS type1
         GROUP BY 1, 2
         HAVING effectiveness != 1.00
         ORDER BY effectiveness DESC, type1.opposing_type
         """
     return f"""
-    WITH types_eff AS (
+    WITH type_eff AS (
         SELECT * 
         FROM "{TYPES_EFF_FILEPATH}" 
         WHERE type IN {tuple(types)}
@@ -67,16 +67,39 @@ def _get_pokemon_types(name: str) -> duckdb.DuckDBPyRelation:
     return duckdb.sql(query).fetchone()[0]
 
 
-def _get_pokemon_type_eff(types: list) -> duckdb.DuckDBPyRelation:
+def _get_pokemon_type_eff(types: list) -> list:
     query = _get_type_effectiveness_query(types)
-    return duckdb.sql(query)
+    return duckdb.sql(query).fetchall()
+
+
+def _format_results(type_eff: list) -> None:
+    formatted_results = ""
+    POKETYPE_ALIGNMENT = len(max([i[0] for i in type_eff], key=len))
+    for i in type_eff:
+        poketype, effectiveness = i[0], i[1]
+        relationships = {
+            "no_damage":"NO DAMAGE from",
+            "resistant":"RESISTANT against",
+            "weak":"WEAK to"
+        }
+        if effectiveness == 0.0:
+            relationship = relationships["no_damage"]
+        elif effectiveness < 1.0:
+            relationship = relationships["resistant"]
+        else:
+            relationship = relationships["weak"]
+        REL_ALIGNMENT = len(max(list(relationships.values()), key=len))
+        # TYPE_ALIGNMENT = 
+        formatted_results += f"{relationship:<{REL_ALIGNMENT}} {poketype.title():<{POKETYPE_ALIGNMENT}}: {effectiveness}\n"
+    return formatted_results
 
 
 @app.command()
-def get_pokemon_type_effectiveness(name: str):
+def get_pokemon_type_effectiveness(name: str) -> None:
     types = _get_pokemon_types(name)
-    results = _get_pokemon_type_eff(types)
-    print(results)
+    type_eff = _get_pokemon_type_eff(types)
+    results = _format_results(type_eff)
+    print(f"\n{results}")
 
 
 if __name__ == "__main__":
