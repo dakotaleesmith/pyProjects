83e9c2b86718761079a1dd9b52af9edf76c8ac5c
dakotaleesmith <dakotaleesmith@gmail.com>
2023-03-12 17:26:49 -0700

    Refactored some of the steps in the main function into their own methods; added two filepath constants; and added a _validate_pokemon method that checks if the pokemon provided matches a name in the data

--- pokemon/get_pokemon.py
+++ pokemon/get_pokemon.py
@@ -1,14 +1,26 @@
+import json
+
 import duckdb
-from rich import print
 import typer
+from rich import print
+
+POKEDATA_FILEPATH = "data/pokedata.json"
+TYPES_EFF_FILEPATH = "data/types_parsed_long.csv"
 
 app = typer.Typer()
 
 
+def _validate_pokemon(name: str) -> bool:
+    with open(POKEDATA_FILEPATH, "r") as f:
+        data = json.load(f)
+    return name in [i.get("name") for i in data]
+
+
 def _get_types_query(name: str) -> str:
+    assert _validate_pokemon(name), "Pokemon name not found. Try checking the spelling."
     return f"""
     SELECT types
-    FROM "data/pokedata.json" 
+    FROM "{POKEDATA_FILEPATH}" 
     WHERE name = '{name}'
     ;
     """
@@ -18,31 +30,30 @@ def _get_type_effectiveness_query(types: list) -> str:
     if len(types) > 1:
         select_stmt = """
         SELECT
-            protag.opposing_type
-            , COALESCE(protag.effectiveness_multiplier * antag.effectiveness_multiplier, 0) AS effectiveness
-        FROM types_eff AS protag
-        INNER JOIN types_eff AS antag
-            ON protag.opposing_type = antag.opposing_type
-            AND protag.type != antag.type
+            type1.opposing_type
+            , COALESCE(type1.effectiveness_multiplier * type2.effectiveness_multiplier, 0) AS effectiveness
+        FROM types_eff AS type1
+        INNER JOIN types_eff AS type2
+            ON type1.opposing_type = type2.opposing_type
+            AND type1.type != type2.type
         GROUP BY 1, 2
         HAVING effectiveness != 1.00
-        ORDER BY effectiveness DESC, protag.opposing_type
+        ORDER BY effectiveness DESC, type1.opposing_type
         """
     else:
         select_stmt = """
         SELECT
-            protag.opposing_type
-            , COALESCE(protag.effectiveness_multiplier, 0) AS effectiveness
-        FROM types_eff AS protag
+            type1.opposing_type
+            , COALESCE(type1.effectiveness_multiplier, 0) AS effectiveness
+        FROM types_eff AS type1
         GROUP BY 1, 2
         HAVING effectiveness != 1.00
-        ORDER BY effectiveness DESC, protag.opposing_type
+        ORDER BY effectiveness DESC, type1.opposing_type
         """
-
     return f"""
     WITH types_eff AS (
         SELECT * 
-        FROM "data/types_parsed_long.csv" 
+        FROM "{TYPES_EFF_FILEPATH}" 
         WHERE type IN {tuple(types)}
     )
 
@@ -51,15 +62,21 @@ def _get_type_effectiveness_query(types: list) -> str:
     """
 
 
+def _get_pokemon_types(name: str) -> duckdb.DuckDBPyRelation:
+    query = _get_types_query(name)
+    return duckdb.sql(query).fetchone()[0]
+
+
+def _get_pokemon_type_eff(types: list) -> duckdb.DuckDBPyRelation:
+    query = _get_type_effectiveness_query(types)
+    return duckdb.sql(query)
+
+
 @app.command()
 def get_pokemon_type_effectiveness(name: str):
-    types_query = _get_types_query(name)
-    types = duckdb.sql(types_query).fetchone()[0]
-
-    type_effectiveness_query = _get_type_effectiveness_query(types)
-    type_effectiveness = duckdb.sql(type_effectiveness_query)
-
-    print(type_effectiveness)
+    types = _get_pokemon_types(name)
+    results = _get_pokemon_type_eff(types)
+    print(results)
 
 
 if __name__ == "__main__":
